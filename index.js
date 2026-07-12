91afd77737d6db6705be14323ac7ce832fcd7387
dinhhaor106 <dinhhaor106@gmail.com>
2026-07-12 10:06:08 +0700

    fix: handle CORS OPTIONS preflight requests

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
